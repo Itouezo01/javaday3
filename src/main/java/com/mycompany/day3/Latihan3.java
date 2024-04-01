6913c41bdcebc56f4e6a4baf3906529e068ccec6
A-17 <A-17@A-17>
2024-04-01 10:41:49 +0700

    inisialisasi awal

---
+++ src/main/java/com/mycompany/day3/Latihan3.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.day3;
+
+/**
+ *
+ * @author A-17
+ */
+public class Latihan3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+    }
+}
